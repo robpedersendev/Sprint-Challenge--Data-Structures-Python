0afe7336d7e7e9829c8c221f44bd808e7e625ee4
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-05-29 20:49:41 -0600

    Started working on the names.py file
    Commented out the nested loops and added a comment of my runtime length and the type of complexity

--- names/names.py
+++ names/names.py
@@ -12,15 +12,19 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+"""
+Runtime of a nested loop is: 6.731910943984985 seconds. It has a runtime complexity of O(n**2)
+"""
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
