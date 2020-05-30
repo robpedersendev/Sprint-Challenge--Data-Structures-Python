98f49416f74f9501118678f1a1cc32184c42cfc6
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-05-29 20:54:47 -0600

    Pasted the code from the Data-Structures project Binary Search Tree file

--- names/names.py
+++ names/names.py
@@ -12,11 +12,53 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
 # # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+
+# Pasted the BST code from other project
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):  # Logorithmic time
+        # If greater than or equal go right
+        if value >= self.value:
+            # check if .right exists
+            if self.right is not None:
+                # if so, make that node call insert with the same value
+                self.right.insert(value)
+            # if not,  create a node with that value, set node as right child
+            else:
+                new_node = BSTNode(value)
+                self.right = new_node
+        # Else, go left!
+        else:
+            if self.left is not None:
+                # if so, make that node call insert with the same value
+                self.left.insert(value)
+            # if not,  create a node with that value, set node as left child
+            else:
+                new_node = BSTNode(value)
+                self.left = new_node
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target > self.value:
+            return self.right.contains(target) if self.right is not None else False
+        else:
+            return self.left.contains(target) if self.left is not None else False
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
